dd171ff84b074b3767cd556fdf1effa712be1010
NityaLalchandani14 <86710408+NityaLalchandani14@users.noreply.github.com>
2021-07-14 19:12:16 +0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+var garden,rabbit, apple;
+var gardenImg,rabbitImg, appleIMG;
+
+function preload(){
+  gardenImg = loadImage("garden.png");
+  rabbitImg = loadImage("rabbit.png");
+  appleImg = loadImage("apple.png");
+}
+
+function setup(){
+  
+  createCanvas(400,400);
+  
+// Moving background
+garden=createSprite(200,200);
+garden.addImage(gardenImg);
+
+//creating boy running
+rabbit = createSprite(180,340,30,30);
+rabbit.scale =0.09;
+rabbit.addImage(rabbitImg);
+}
+
+
+function draw() {
+  background(0);
+  rabbit.x = World.mouseX;
+  edges= createEdgeSprites();
+  rabbit.collide(edges);
+
+  drawSprites();
+}
+function createApples(){
+  apple = createSprite (randomNumber(1-400), 0, 30, 30)
+  apple.addImage (appleIMG)
+  apple.scale= 0.1
+  apple.velocityY=1
+}
